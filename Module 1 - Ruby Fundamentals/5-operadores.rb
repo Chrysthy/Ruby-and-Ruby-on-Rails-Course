6052defc3d55e12d0f287d18be898952819e6865
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-26 15:33:41 -0300

    add Operadores de Comparação

--- Module 1 - Ruby Fundamentals/5-operadores.rb
+++ Module 1 - Ruby Fundamentals/5-operadores.rb
@@ -14,6 +14,7 @@ exp = num1 ** num2 #exponenciação
 
 puts sum, subt, divs, mult, mod, exp
 
+
 #Operadores de Atribuição
 num1 += 1 # num1 = num1 + 1
 num1 -= 1 # num1 = num1 - 1
@@ -32,3 +33,12 @@ a, b = 5, 4
 a, b = b, a
 
 puts  a, b
+
+
+#Operadores de Comparação
+bigger = num1 > num2
+smaller = num1 < num2
+equal = num1 == num2
+different = num1 != num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
