e1fbff006d782c8c9575d695c44136b7ddfd840d
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-26 15:22:50 -0300

    add Operadores Aritméticos

---
+++ Module 1 - Ruby Fundamentals/5-operadores.rb
@@ -0,0 +1,13 @@
+puts "Digite o primeiro número:"
+num1 = gets.chomp.to_i
+
+puts "Digite o segundo número:"
+num2 = gets.chomp.to_i
+
+#Operadores Aritméticos
+sum = num1 + num2
+subt = num1 - num2
+divs = num1 / num2
+mult = num1 * num2
+
+puts sum, subt, divs, mult
