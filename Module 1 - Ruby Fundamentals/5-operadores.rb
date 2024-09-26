5464c799db14ff1b139f9ccf365e05356b655825
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-26 15:42:11 -0300

    add Operadores lógicos

--- Module 1 - Ruby Fundamentals/5-operadores.rb
+++ Module 1 - Ruby Fundamentals/5-operadores.rb
@@ -42,3 +42,8 @@ equal = num1 == num2
 different = num1 != num2
 bigger_equal = num1 >= num2
 smaller_equal = num1 <= num2
+
+
+#Operadores lógicos
+puts ((2 > 4) and (3 > 1)) # os dois precisam ser verdadeiros para dar TRUE, se um for false o retorno é FALSE
+puts ((2 > 4) or (3 > 1)) #se um deles for verdadeiro, retorna TRUE
