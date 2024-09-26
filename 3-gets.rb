12c65d462d376813c7ff46c41ed143acc9a496b0
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-09-26 14:07:39 -0300

    add comments

--- 3-gets.rb
+++ 3-gets.rb
@@ -9,9 +9,11 @@ puts "Informe o ano do filme:"
 
 yearLaunch = gets.chomp.to_i
 puts yearLaunch
+#to_i = converte para números inteiros
 #puts yearLaunch.class
 
 puts "Informe o preço do filme:"
 price = gets.chomp.to_f
 puts price
+#to_f = converte para números quebrados
 # puts price.class
