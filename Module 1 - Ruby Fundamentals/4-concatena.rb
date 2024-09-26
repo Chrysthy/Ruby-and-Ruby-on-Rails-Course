8b742a6cfc5ed6c0133502410e6003aeb3ac77cc
Chrystine Martins <chrystine.martins@outlook.com>
2024-09-26 19:13:22 -0300

    update content

--- Module 1 - Ruby Fundamentals/4-concatena.rb
+++ Module 1 - Ruby Fundamentals/4-concatena.rb
@@ -1,17 +1,17 @@
 #Concatenação e exibição dos valoes
 puts "Informe o nome do filme:"
 name = gets.chomp
-puts name
+# puts name
    
 
 puts "Informe o ano do filme:"
 yearLaunch = gets.chomp.to_i
-puts yearLaunch
+# puts yearLaunch
 
        
 puts "Informe o preço do filme:"
 price = gets.chomp.to_f
-puts price
+# puts price
 
 
 #1ª forma de fazer usando o sinal de +
@@ -19,23 +19,23 @@ puts "Nome do filme: " + name
 puts "Ano de Lançamento " + yearLaunch.to_s
 puts "Preço do filme: " + price.to_s
 
-#colocamos to_s para converte de números para string
-#é necessário colocar para não dar erro quando rodar o código, pois gets.chomp sempre retorna uma string
+# #colocamos to_s para converte de números para string
+# #é necessário colocar para não dar erro quando rodar o código, pois gets.chomp sempre retorna uma string
 
-#2ª forma de fazer usando #{}
+# #2ª forma de fazer usando #{}
 puts "Nome do filme: #{name}"
 puts "Ano de lançamento: #{yearLaunch}"
 puts "Preço do filme: #{price}"
     
-#Utilizando a forma 2, não é necessário fazer a converção com o .to_s
+# #Utilizando a forma 2, não é necessário fazer a converção com o .to_s
     
-#3ª forma de fazer usando um único puts
+# #3ª forma de fazer usando um único puts
 puts "Nome do filme: #{name}, Ano de Lançamento: #{yearLaunch}, Preço do filme: #{price}"
 puts "Nome do filme: #{name}\n Ano de Lançamento: #{yearLaunch}\n Preço do filme: #{price}"
 
-#podemos usar o \n para quebar as linhas. É necessário tirar as vírgulas
+# #podemos usar o \n para quebar as linhas. É necessário tirar as vírgulas
 
-#4ª forma de fazer usando uma string Multilinha
+# #4ª forma de fazer usando uma string Multilinha
 puts <<~MULTILINE_STRING
 
     Nome do filme: #{name}
