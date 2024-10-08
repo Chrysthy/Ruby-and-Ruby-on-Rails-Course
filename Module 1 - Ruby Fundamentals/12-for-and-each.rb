951cd1f0476903fff63b950f0fcb33dbf7302295
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-10-08 13:42:26 -0300

    add example each do

--- Module 1 - Ruby Fundamentals/12-for-and-each.rb
+++ Module 1 - Ruby Fundamentals/12-for-and-each.rb
@@ -1,13 +1,24 @@
 # Alternativa 1 - Usando o for
-for i in 1..5  #vai estar iterando esses itens de 1 a 5 e a cada iteração jopa o valor para variável i
+# for i in 1..5  #vai estar iterando esses itens de 1 a 5 e a cada iteração jopa o valor para variável i
 
-    puts i
+#     puts i
 
-end
+# end
 
 # Alternativa 2 - Usando o each
-(1..5).each do |i|
+# (1..5).each do |i|
 
-    puts i
+#     puts i
+
+# end
+
+moviesList = ["Titanic", "Resident Evil", "Silent Hill", "Drácula"] 
+
+# 1-Iterando valores de um Array
+moviesList.each do |movie|  #Chama o array.each do, passa a variavél de controle em barra vertical e depois chama a variável para apresentar as informações
+
+    puts movie
 
 end
+
+
