4e01b8b6eacc32a53c704f11d8315705b96ab784
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-10-08 13:27:57 -0300

    add new example while

--- Module 1 - Ruby Fundamentals/11-while.rb
+++ Module 1 - Ruby Fundamentals/11-while.rb
@@ -1,8 +1,36 @@
-i = 0
+# i = 0
 
-while i < 5
+# while i <= 5
     
-    puts i
-    i += 1 #precisa colocar para parar o loop
+#     puts i
+#     i += 1 #precisa colocar para parar o loop
 
-end
+# end
+
+#Exemplo 2
+puts "Digite o nome do filme:"
+
+    movie_name = gets.chomp
+    qtd_rating = 0
+    total_rating = 0
+    rating = 0
+    average = 0
+    
+    while rating != -1
+    
+        puts "Informe a nota do filme:"
+        rating = gets.chomp.to_i
+    
+        if rating != -1
+    
+            total_rating += rating # total_rating = total_rating + rating
+            
+            qtd_rating += 1
+    
+            average = total_rating / qtd_rating
+    
+        end
+    
+    end
+    
+puts "Média das avaliações do filme #{movie_name} é %.2f" % average
