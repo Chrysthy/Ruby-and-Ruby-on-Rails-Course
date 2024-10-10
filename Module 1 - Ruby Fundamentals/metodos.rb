5d37404a13d48be6720e5f09092a0df972116d5a
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-10-10 15:54:44 -0300

    add metodo def

---
+++ Module 1 - Ruby Fundamentals/metodos.rb
@@ -0,0 +1,8 @@
+#Quando se tem uma parte do código com muita recorrência, ao invés de duplicar o código, usamos o def e chamamos a função no final para converter o código em um método
+def Hello
+
+    puts "Hello World"
+
+end
+
+Hello()
