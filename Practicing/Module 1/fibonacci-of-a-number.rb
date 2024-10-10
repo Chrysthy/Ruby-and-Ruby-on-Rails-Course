03cc442b334cf955b14759023f55f424763554f9
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2024-10-10 15:33:13 -0300

    add variavel and while

--- Practicing/Module 1/fibonacci-of-a-number.rb
+++ Practicing/Module 1/fibonacci-of-a-number.rb
@@ -1,3 +1,19 @@
+# puts "Digite um número:"
+# number = gets.chomp.to_i
+
+# fibonacci = 1
+# valorAnteriroFibonacci = 0
+
+# while fibonacci <= number
+#   puts fibonacci
+
+#   temp = fibonacci
+#   fibonacci += valorAnteriroFibonacci
+
+#   valorAnteriroFibonacci = temp
+
+# end
+
 puts "Digite um número:"
 number = gets.chomp.to_i
 
@@ -5,11 +21,5 @@ fibonacci = 1
 valorAnteriroFibonacci = 0
 
 while fibonacci <= number
-  puts fibonacci
 
-  temp = fibonacci
-  fibonacci += valorAnteriroFibonacci
-
-  valorAnteriroFibonacci = temp
-
-end
+    puts fibonacci
