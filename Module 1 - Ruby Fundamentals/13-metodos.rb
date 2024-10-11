1df700e3bf592e7a9316fb176fbd6b30030e248f
Chrystine Martins <chrystine.martins@outlook.com>
2024-10-11 20:38:46 -0300

    update file name

---
+++ Module 1 - Ruby Fundamentals/13-metodos.rb
@@ -0,0 +1,20 @@
+# 1-Método para imprimir 
+#Quando se tem uma parte do código com muita recorrência, ao invés de duplicar o código, usamos o def e chamamos a função no final para converter o código em um método
+def Hello
+
+    puts "Hello World"
+
+end
+
+Hello() #uma vez que tem o método, pode executar quantas vezes quiser
+Hello() 
+Hello() 
+
+# 2-Método para somar dois números
+def sum()
+    
+    puts 5 + 1
+
+end
+
+sum()
