404385e6159fc02a62c20f8aee07689d43092753
Chrystine Martins <chrystine.martins@outlook.com>
2024-10-11 20:54:26 -0300

    add metodo com parâmetro

--- Module 1 - Ruby Fundamentals/14-metodos-parametros.rb
+++ Module 1 - Ruby Fundamentals/14-metodos-parametros.rb
@@ -1,2 +1,9 @@
 # 1-Método que recebe dois parâmetros sobre nome
 def fullName(fname, lname)
+
+  puts "Nome completo: #{fname} #{lname}"
+
+end
+
+fullName("Chrystine", "Martins")
+fullName("Noob", "Saibot")
